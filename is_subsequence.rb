4051d6de095836644d51bfadd8d79b737c1cef8d
Buck Melton <buckm@alumni.stanford.edu>
2024-08-26 14:58:49 -0700

    Add my notes, pseudocode

--- is_subsequence.rb
+++ is_subsequence.rb
@@ -21,3 +21,43 @@
 # 0 <= s.length <= 100
 # 0 <= t.length <= 104
 # s and t consist only of lowercase English letters.
+
+=begin
+MY NOTES:
+
+This is specified as a two-pointer problem.
+
+Note that, in the case where we DO find that s is a subsequence of t, we don't have to remember/return
+the positions of the letters of s in t.
+
+First thought: s_ptr points to the letter in s that we are currently seeking in t. t_ptr points to the position in t
+of the farthest / most recent letter we've successfully found from the substring.
+
+But then I thought, we may need to backtrack more than just the most recently successfully discovered.
+For instance if s = 'abcded' and t = 'abcabcdabcdeabcdbdcedcef', we'll often need to backtrack past occurences of finding e, d etc before finding f.
+
+But then I realized that's BS, I was right the first time, we do literally only need to keep track of the last
+letter successfully found.
+
+PSEUDOCODE:
+initialize s_ptr to first letter in s
+initialize t_ptr to first letter in t
+while s_ptr < s.length
+  while t[t_ptr] != s[s_ptr] && t_ptr < t.length
+    increment t_ptr
+  if t_ptr >= t.length
+    Assert: we've looked through the entire
+    return false
+  assert: we have found s[s_ptr] in t
+  if s_ptr == s.length-1
+    Assert: we've found the last letter
+    return true
+  set t_ptr (now pointing to the farthest s letter we've found) to the next letter in t, i.e. increment t_ptr
+  increment s_ptr, since we've just found the s letter we were looking for, we need to go on and look for the next one
+I *think* we are guaranteed to have returned by now
+Assert: we should never get here
+=end
+
+def is_subsequence(s, t)
+
+end
