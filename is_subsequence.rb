66919fc11085ecd72c699d642253f0ee07771c6d
Buck Melton <buckm@alumni.stanford.edu>
2024-08-27 08:36:14 -0700

    Implement solution, confirmed on easy test cases

--- is_subsequence.rb
+++ is_subsequence.rb
@@ -27,8 +27,7 @@ MY NOTES:
 
 This is specified as a two-pointer problem.
 
-Note that, in the case where we DO find that s is a subsequence of t, we don't have to remember/return
-the positions of the letters of s in t.
+Note that, in the case where we DO find that s is a subsequence of t, we don't have to remember/return the positions of the letters of s in t.
 
 First thought: s_ptr points to the letter in s that we are currently seeking in t. t_ptr points to the position in t
 of the farthest / most recent letter we've successfully found from the substring.
@@ -36,8 +35,7 @@ of the farthest / most recent letter we've successfully found from the substring
 But then I thought, we may need to backtrack more than just the most recently successfully discovered.
 For instance if s = 'abcded' and t = 'abcabcdabcdeabcdbdcedcef', we'll often need to backtrack past occurences of finding e, d etc before finding f.
 
-But then I realized that's BS, I was right the first time, we do literally only need to keep track of the last
-letter successfully found.
+But then I realized that's BS, I was right the first time, we do literally only need to keep track of the last letter successfully found.
 
 PSEUDOCODE:
 initialize s_ptr to first letter in s
@@ -46,7 +44,7 @@ while s_ptr < s.length
   while t[t_ptr] != s[s_ptr] && t_ptr < t.length
     increment t_ptr
   if t_ptr >= t.length
-    Assert: we've looked through the entire
+    Assert: we've looked through the entire t
     return false
   assert: we have found s[s_ptr] in t
   if s_ptr == s.length-1
@@ -59,5 +57,15 @@ Assert: we should never get here
 =end
 
 def is_subsequence(s, t)
-
+  s_ptr = t_ptr = 0
+  while s_ptr < s.length do
+    while ((t[t_ptr] != s[s_ptr]) && t_ptr < t.length) do
+      t_ptr += 1
+    end
+    return false if t_ptr >= t.length
+    return true if s_ptr == s.length - 1
+    t_ptr += 1
+    s_ptr += 1
+  end
+  p "We should never get here"
 end
